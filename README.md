f62dca84225c76d77c9b43fbcdb115ca71a27aa3
Tom Parsons <thomaspatrickparsons@gmail.com>
2017-05-09 18:30:05 +0100

    updated read me, removed spaces - LINT

--- README.md
+++ README.md
@@ -16,4 +16,8 @@ if you want minified and obfuscated version
 webpack --progress -p
 ```
 
-Note: make sure you have webpack and babel-cli installed (globally)
+Note: make sure you have webpack, webpack-dev-server and babel-cli installed (globally)
+
+```npm -g i webpack webpack-dev-server babel-cli```
+
+will run on: `http://localhost:3333/`
--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import 'whatwg-fetch'
 const _compose = (f,g) => (x) => g(f(x))
 
 class App extends React.Component {
-
   componentDidMount() {
     let ph = document.body.querySelectorAll('#yourwelcomewidget')
     let root = document.querySelectorAll('#yourwelcome')[0]
     if (ph.length !== 0) ph[0].appendChild(root)
   }
 
- 
   render() {
-
     return (
       <div style={{ background: 'green' }}>
-        <h1> Welcome to your welcome </h1>
+        <h1>Welcome to your welcome </h1>
       </div>
     )
-
   }
-
 }
 
 export default App
